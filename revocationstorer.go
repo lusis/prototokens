7ca5897e8473ef1d01f1e7f00061ea95938a7443
lusis <lusis.org+github.com@gmail.com>
2023-05-11 14:27:42 -0400

    lint errors

--- revocationstorer.go
+++ revocationstorer.go
@@ -20,11 +20,11 @@ type UnimplementedRevocationStorer struct{}
 // Revoke revokes a [tokenpb.ProtoToken] by its identifier
 // revocationID does not have to be the token's id or sid even
 // you can just use the signature if you want
-func (urs *UnimplementedRevocationStorer) Revoke(ctx context.Context, revocationID string) error {
+func (urs *UnimplementedRevocationStorer) Revoke(_ context.Context, _ string) error {
 	return ErrUnimplemented
 }
 
 // CheckRevocation checks the store to see if the token has been revoked
-func (urs *UnimplementedRevocationStorer) CheckRevocation(ctx context.Context, revocatonID string) error {
+func (urs *UnimplementedRevocationStorer) CheckRevocation(_ context.Context, _ string) error {
 	return ErrUnimplemented
 }
